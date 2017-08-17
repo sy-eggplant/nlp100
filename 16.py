fa15b2e2c241d0fce44b6b215b34c0c5f7dc835d
yoshikawa.shoko@slogan.jp <yoshikawa.shoko@slogan.jp>
2017-08-17 19:43:39 +0900

    16.pyを追加

--- 16.py
+++ 16.py
@@ -1,18 +1,20 @@
-import linecache
+import math
 
 f = open('hightemp.txt')
 num = int(input('>'))
 total = len(f.readlines())
-surplus = total % num
-date_num = total//num+1
-date = ""
+f.close()
+f = open('hightemp.txt')
+
 line = f.readline()
+
 print(line)
-for i in range(total):
-  line = f.readline()
-  date += line
-  print(line)
+for i in range(math.ceil(total/num)):
+    fw = open('output'+str(i)+'.txt','w')
+    for j in range(num):
+        fw.writelines(line)
+        line=f.readline()
+        print(line)
+    fw.close()
 
-
-print(date)
 f.close()
