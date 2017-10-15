937cb689ee3206b4aa451740895cd8df122fa97e
sy-eggplant <sy.eggplant@gmail.com>
2017-10-15 22:08:26 +0900

    動のみ抽出

--- 31.py
+++ 31.py
@@ -1,4 +1,10 @@
 import neko_mecab
 
+mapping = neko_mecab.neko_mecab()
 
-neko_mecab.neko_mecab()
+verbs = []
+for value in mapping:
+    if value['pos'] == '動詞':
+        verbs.append(value['surface'])
+
+print(verbs)
