a22424bf48b2a20a05e62b5f18abc77aa2f0c393
sy-eggplant <sy.eggplant@gmail.com>
2017-10-15 21:54:40 +0900

    関数にした

--- 30.py
+++ 30.py
@@ -1,18 +1,23 @@
-f = open('neko.txt.mecab', 'r')
 
-mapping = []
 
-for line in f:
-    if line.find("EOS") == -1:
-        hyou ,other = line.split("\t")
-        other = other.split(",")
-        dic = {
-            'surface': hyou,
-            'base': other[6],
-            'pos': other[0],
-            'pos1': other[1]
-        }
-        mapping.append(dic)
+def neko_mecab():
+    f = open('neko.txt.mecab', 'r')
+    mapping = []
 
-print(mapping)
-f.close()
+    for line in f:
+        if line.find("EOS") == -1:
+            hyou ,other = line.split("\t")
+            other = other.split(",")
+            dic = {
+                'surface': hyou,
+                'base': other[6],
+                'pos': other[0],
+                'pos1': other[1]
+            }
+            mapping.append(dic)
+    f.close()
+    return mapping
+
+
+
+print(neko_mecab())
