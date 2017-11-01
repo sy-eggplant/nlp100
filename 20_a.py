10f2d56a622353e9a1568d3c1bc3e809836de561
yoshikawa.shoko@slogan.jp <yoshikawa.shoko@slogan.jp>
2017-11-01 19:15:30 +0900

    EOSありでも辞書に保存されるように変更

--- 20_a.py
+++ 20_a.py
@@ -1,10 +1,9 @@
-
 import json
 
 with open("jawiki-country.json") as f:
     article_json = f.readline()
     while article_json:
         article_dict = json.loads(article_json)
-        if article_dict["title"] == u"イギリス":
+        if article_dict["title"] == "イギリス":
             print(article_dict["text"])
         article_json = f.readline()
--- neko_mecab.py
+++ neko_mecab.py
@@ -3,9 +3,22 @@
 def neko_mecab():
     f = open('neko.txt.mecab', 'r')
     mapping = []
-
     for line in f:
-        if line.find("EOS") == -1:
+        num = line.find('EOS')
+        if num == 0:
+            sent = line.lstrip('EOS')
+            line = sent
+            if len(line.split("\t")) >= 2:
+                hyou ,other = line.split("\t")
+                other = other.split(",")
+                dic = {
+                    'surface': hyou,
+                    'base': other[6],
+                    'pos': other[0],
+                    'pos1': other[1]
+                }
+                mapping.append(dic)
+        if num == -1:
             hyou ,other = line.split("\t")
             other = other.split(",")
             dic = {
@@ -19,5 +32,5 @@ def neko_mecab():
     return mapping
 
 
-
-# print(neko_mecab())
+#neko_mecab()
+print(neko_mecab())
