96e8f8d7119e7e56f56f30c8ed495a4d77b51167
sy-eggplant <sy.eggplant@gmail.com>
2018-03-17 12:15:00 +0900

    ファイル名を変更

--- txt_to_mecab.py
+++ txt_to_mecab.py
@@ -1,8 +1,8 @@
 from natto import MeCab
 
 mc = MeCab()
-f_input = open('neko.txt')
-f_output = open('neko2.txt.mecab', 'w')
+f_input = open('test.txt')
+f_output = open('test.txt.mecab', 'w')
 
 line = f_input.readline()
 data = ""
