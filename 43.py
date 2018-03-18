33c32ce13008b6f158d8b2f75755abc8bde973b7
sy-eggplant <sy.eggplant@gmail.com>
2018-03-18 21:09:18 +0900

    43

---
+++ 43.py
@@ -0,0 +1,64 @@
+import morph
+import chunk
+
+m_objs = []
+m_obj = []
+srcs = {}
+sentences = []
+sentence = []
+c = None
+with open('neko.txt.cabocha', encoding='utf-8') as input_file:
+    for line in input_file:
+        line_elements = line.split()
+        if line_elements[0] == 'EOS':
+            #一文の終わり
+            if c is not None:
+                sentence.append(c)
+            if len(sentence)>0:
+                sentences.append(sentence)
+            m_objs.append(m_obj)
+            #初期化
+            m_obj = []
+            srcs = {}
+            sentence = []
+            c = None
+
+        elif  line_elements[0] == '*':
+            #文節の終わり
+            if c is not None:
+                sentence.append(c)
+            # chunkの要素を取得
+            dst = int(line_elements[2].replace('D',''))
+            #src（かかり元文節インデックス番号）
+            src = int(line_elements[1])
+
+            if dst == -1:
+                #chunk作成
+                c = chunk.Chunk(morphs=[], srcs=[],dst=None)
+                srcs = None
+            else:
+                #chunk作成
+                c = chunk.Chunk(morphs=[], srcs=[],dst=dst)
+                if dst in srcs:
+                    srcs[dst].append(src)
+                else:
+                    srcs[dst] = [src]
+                if src in srcs:
+                    c.srcs.append(srcs[src])
+        else:
+            #単語ごと
+            l = line_elements[0].split(',') + line_elements[1].split(',')
+            if l[0] == '記号':
+                pass
+            elif l[1] == '記号':
+                pass
+            else:
+                m = morph.Morph(surface=l[0], base=l[7], pos=l[1], pos1=l[2])
+                m_obj.append(m)
+                c.morphs.append(m)
+
+for target in sentences:
+    for chunk_obj in target:
+        if chunk_obj.dst is not None:
+            if chunk_obj.has_noun() and target[chunk_obj.dst].has_verb():
+                print(chunk_obj.surface() + '   ' + target[chunk_obj.dst].surface())
--- chunk.py
+++ chunk.py
@@ -4,5 +4,20 @@ class Chunk:
         self.morphs = morphs
         self.srcs = srcs
         self.dst = dst
+
     def surface(self):
         return ''.join(m.surface for m in self.morphs)
+
+    def has_noun(self):
+        result = False
+        for m in self.morphs:
+            if m.pos == '名詞':
+                result = True
+        return result
+
+    def has_verb(self):
+        result = False
+        for m in self.morphs:
+            if m.pos == '動詞':
+                result = True
+        return result
